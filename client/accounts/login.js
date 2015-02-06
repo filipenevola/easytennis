23f059381f3dea6bfc88c515f07cf9c56f8ef039
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2015-02-06 11:59:47 -0200

    callback for social logins

--- client/accounts/login.js
+++ client/accounts/login.js
@@ -2,6 +2,16 @@ var isLoginServiceAvailable = function (serviceName) {
     return ServiceConfiguration.configurations.find({service: serviceName}).count() > 0;
 };
 
+
+var loginCallback = function (err) {
+    if (err) {
+        toast('Ops! ' + err.reason, 4000);
+    } else {
+        Router.go('Dashboard');
+        return true;
+    }
+};
+
 Template.Login.events({
     'click #login': function (e, t) {
         e.preventDefault();
@@ -12,32 +22,24 @@ Template.Login.events({
             toast('Ops! Please, fill all fields!', 4000);
             return false;
         }
-
-        Meteor.loginWithPassword(email, password, function (err) {
-            if (err) {
-                toast('Ops! ' + err.reason, 4000);
-            } else {
-                Router.go('Dashboard');
-                return true;
-            }
-        });
+        Meteor.loginWithPassword(email, password, loginCallback);
 
         return false;
     },
 
     'click #login-facebook': function (e, t) {
         e.preventDefault();
-        Meteor.loginWithFacebook();
+        Meteor.loginWithFacebook(loginCallback);
     },
 
     'click #login-twitter': function (e, t) {
         e.preventDefault();
-        Meteor.loginWithTwitter();
+        Meteor.loginWithTwitter(loginCallback);
     },
 
     'click #login-google': function (e, t) {
         e.preventDefault();
-        Meteor.loginWithGoogle();
+        Meteor.loginWithGoogle(loginCallback);
     }
 });
 
